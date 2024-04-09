7cc0b573195813e89d0445de4e9af6b8a99d4f7f
swotlycto <cto@swotly.io>
2024-04-09 19:24:55 +0100

    Refactoring: Field renames

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,32 +2,31 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private int _playerOneScore;
+        private int _playerTwoScore;
+        private string _playerOneName;
+        private string _playerTwoName;
 
-        public TennisGame1(string player1Name, string player2Name)
+        public TennisGame1(string playerOneName, string playerTwoName)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _playerOneName = playerOneName;
+            _playerTwoName = playerTwoName;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                _playerOneScore += 1;
             else
-                m_score2 += 1;
+                _playerTwoScore += 1;
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            var score = "";
+            if (_playerOneScore == _playerTwoScore)
             {
-                switch (m_score1)
+                switch (_playerOneScore)
                 {
                     case 0:
                         score = "Love-All";
@@ -44,9 +43,9 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (_playerOneScore >= 4 || _playerTwoScore >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = _playerOneScore - _playerTwoScore;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -56,8 +55,9 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    var tempScore = 0;
+                    if (i == 1) tempScore = _playerOneScore;
+                    else { score += "-"; tempScore = _playerTwoScore; }
                     switch (tempScore)
                     {
                         case 0:
