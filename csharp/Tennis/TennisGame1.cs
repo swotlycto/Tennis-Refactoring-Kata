7e67ed694e5040f0c70ceb194652df3ff74c0881
swotlycto <cto@swotly.io>
2024-04-09 20:19:35 +0100

    Refactoring: Introduced Strategy pattern for drawing scores

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Tennis
 {
@@ -6,11 +7,18 @@ namespace Tennis
     {
         private readonly Player _playerOne;
         private readonly Player _playerTwo;
+        private readonly IStrategy _strategy;
 
         public TennisGame1(string playerOneName, string playerTwoName)
         {
             _playerOne = new Player(playerOneName);
             _playerTwo = new Player(playerTwoName);
+
+            _strategy = new CompositeStrategy(
+                new LoveAllStrategy(_playerOne, _playerTwo),
+                new FifteenAllStrategy(_playerOne, _playerTwo),
+                new ThirtyAllStrategy(_playerOne, _playerTwo),
+                new DeuceStrategy(_playerOne, _playerTwo));
         }
 
         public void WonPoint(string playerName)
@@ -26,8 +34,8 @@ namespace Tennis
 
             if (name.Equals(_playerTwo.Name))
                 return _playerTwo;
-            
-            
+
+
             throw new ArgumentException($"Player '{name}' isn't recognised");
         }
 
@@ -36,24 +44,10 @@ namespace Tennis
             var score = "";
             if (_playerOne.Score == _playerTwo.Score)
             {
-                switch (_playerOne.Score)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                return _strategy.GetScore();
             }
-            else if (_playerOne.Score >= 4 || _playerTwo.Score >= 4)
+            
+            if (_playerOne.Score >= 4 || _playerTwo.Score >= 4)
             {
                 var minusResult = _playerOne.Score - _playerTwo.Score;
                 if (minusResult == 1) score = $"Advantage {_playerOne.Name}";
@@ -67,7 +61,12 @@ namespace Tennis
                 {
                     int tempScore;
                     if (i == 1) tempScore = _playerOne.Score;
-                    else { score += "-"; tempScore = _playerTwo.Score; }
+                    else
+                    {
+                        score += "-";
+                        tempScore = _playerTwo.Score;
+                    }
+
                     switch (tempScore)
                     {
                         case 0:
@@ -85,8 +84,117 @@ namespace Tennis
                     }
                 }
             }
+
             return score;
         }
     }
-}
 
+    public class LoveAllStrategy : IStrategy
+    {
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
+
+        public LoveAllStrategy(Player playerOne, Player playerTwo)
+        {
+            _playerOne = playerOne;
+            _playerTwo = playerTwo;
+        }
+
+        public string GetScore()
+        {
+            if (_playerOne.Score == _playerTwo.Score && _playerOne.Score == 0)
+            {
+                return "Love-All";
+            }
+
+            return null;
+        }
+    }
+
+    public class FifteenAllStrategy : IStrategy
+    {
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
+
+        public FifteenAllStrategy(Player playerOne, Player playerTwo)
+        {
+            _playerOne = playerOne;
+            _playerTwo = playerTwo;
+        }
+
+        public string GetScore()
+        {
+            if (_playerOne.Score == _playerTwo.Score && _playerOne.Score == 1)
+            {
+                return "Fifteen-All";
+            }
+
+            return null;
+        }
+    }
+
+    public class ThirtyAllStrategy : IStrategy
+    {
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
+
+        public ThirtyAllStrategy(Player playerOne, Player playerTwo)
+        {
+            _playerOne = playerOne;
+            _playerTwo = playerTwo;
+        }
+
+        public string GetScore()
+        {
+            if (_playerOne.Score == _playerTwo.Score && _playerOne.Score == 2)
+            {
+                return "Thirty-All";
+            }
+
+            return null;
+        }
+    }
+
+    public class DeuceStrategy : IStrategy
+    {
+        private readonly Player _playerOne;
+        private readonly Player _playerTwo;
+
+        public DeuceStrategy(Player playerOne, Player playerTwo)
+        {
+            _playerOne = playerOne;
+            _playerTwo = playerTwo;
+        }
+
+        public string GetScore()
+        {
+            if (_playerOne.Score == _playerTwo.Score && _playerOne.Score > 2)
+            {
+                return "Deuce";
+            }
+
+            return null;
+        }
+    }
+
+    public interface IStrategy
+    {
+        string GetScore();
+    }
+
+    public class CompositeStrategy : IStrategy
+    {
+        private readonly IStrategy[] _strategies;
+        
+        public CompositeStrategy(params IStrategy[] strategies)
+        {
+            _strategies = strategies;
+        }
+        
+        public string GetScore()
+        {
+            return _strategies.Select(strategy => strategy.GetScore()).FirstOrDefault(score => score != null);
+        }
+    }
+
+}
